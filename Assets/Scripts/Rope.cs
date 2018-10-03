f94d90af95d173fa949d5aa3dcd04f1149fd09f9
NeilHansen <neil.hansen17@gmail.com>
2018-10-03 04:20:49 -0400

    added simple crane movement project and whacky rope physics

---
+++ Assets/Scripts/Rope.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rope : MonoBehaviour {
+
+    internal Rigidbody mybody;
+     
+	internal void Start () {
+        this.gameObject.AddComponent<Rigidbody>();
+        this.mybody = this.gameObject.GetComponent<Rigidbody>();
+        this.mybody.isKinematic = true;
+
+        int childcount = this.transform.childCount;
+
+        for(int i = 0; i < childcount; i++)
+        {
+            Transform trans = this.transform.GetChild(i);
+
+            trans.gameObject.AddComponent<HingeJoint>();
+
+            HingeJoint hinge = trans.gameObject.GetComponent<HingeJoint>();
+
+            hinge.connectedBody =
+                i == 0 ? this.mybody :
+                this.transform.GetChild(i - 1).GetComponent<Rigidbody>();
+
+            hinge.useSpring = true;
+            hinge.enableCollision = true;
+        
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
